ca08d67f42162ac19bee44ac9253f11e38663471
outrun32 <57797470+outrun32@users.noreply.github.com>
2020-12-21 10:55:01 +0400

    Server #3
    Most likely, server part is ready, but there's a lot of bygs i have to fix

--- Server_Part/server_main.py
+++ Server_Part/server_main.py
@@ -9,8 +9,8 @@ def rewrite_database(updated):
 
 app = Flask("__main__")
 
-@app.route('/data', methods=['POST', 'GET'])
-def data_appending():
+@app.route('/data_append', methods=['POST', 'GET'])
+def data_append():
     f = open(name)
     current_json = json.load(f)
     f.close()
@@ -19,9 +19,20 @@ def data_appending():
         current_json['users'].append(input_json)
         rewrite_database(current_json)
         return str(current_json)
-    else:
-        return str(current_json)
-
+    if request.method == "GET":
+        return str(current_json)       
+@app.route('/data_delete', methods = ['DELETE'])
+def data_delete():
+    f = open(name)
+    current_json = json.load(f)
+    f.close()
+    user_ip = str(request.remote_addr)
+    for i in range(len(current_json['users'])):
+        if current_json['users'][i]['ip'] == user_ip:
+            del current_json['users'][i]
+            break
+    rewrite_database(current_json)
+    return str(current_json)    
 @app.route('/')
 def index():
     with open(name, "r") as reader:
--- Server_Part/test_requests.py
+++ Server_Part/test_requests.py
@@ -3,6 +3,10 @@ import requests
 import socket
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
-dictToSend = {"local":str(local_ip)}
-res = requests.post('http://192.168.43.84:5000/data', json=dictToSend)
-print(res.text)
+dictToSend = {"ip":str(local_ip)}
+def add():
+    res = requests.post('http://192.168.43.84:5000/data_append', json=dictToSend)
+    print(res.text)
+def delete():
+    res = requests.delete('http://192.168.43.84:5000/data_delete')
+    print(res.text)
