aac0e17f3f0e2c895638f3ec10d99a48f5506a19
outrun32 <57797470+outrun32@users.noreply.github.com>
2020-12-21 09:27:35 +0400

    Added option to host from one network

--- Server_Part/server_main.py
+++ Server_Part/server_main.py
@@ -2,25 +2,25 @@ from flask import Flask, jsonify, request
 import json
 
 name = "users_data.json"
-users = {'user':"localhost"}
 
-def initialize_on_start(name):
+def rewrite_database(updated):
     with open(name, 'w') as outfile:
-        json.dump(users, outfile)
+        json.dump(updated, outfile)
 
 app = Flask("__main__")
 
-@app.route('/tests/endpoint', methods=['POST', 'GET'])
+@app.route('/data', methods=['POST', 'GET'])
 def data_appending():
+    f = open(name)
+    current_json = json.load(f)
+    f.close()
     if request.method == "POST":
         input_json = request.get_json(force=True)
-        f = open(name)
-        current_json = json.load(f)
-        f.close()
-        current_json.update(input_json)
-        return current_json
+        current_json['users'].append(input_json)
+        rewrite_database(current_json)
+        return str(current_json)
     else:
-        return "asfgsgfd"
+        return str(current_json)
 
 @app.route('/')
 def index():
@@ -28,5 +28,4 @@ def index():
         return reader.read()
 
 if __name__ == "__main__":
-    initialize_on_start(name)
-    app.run()
+    app.run(host = '192.168.43.84',debug=True)
--- Server_Part/test_requests.py
+++ Server_Part/test_requests.py
@@ -1,6 +1,8 @@
 import json
 import requests
-dictToSend = {"gay":'me'}
-res = requests.post('http://127.0.0.1:5000/tests/endpoint', json=dictToSend)
+import socket
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+dictToSend = {"local":str(local_ip)}
+res = requests.post('http://192.168.43.84:5000/data', json=dictToSend)
 print(res.text)
-#dictFromServer = res.json()
